22684f338e4a55f57558d086d7790c67a252aa28
Jan Kaul <jankaul@mailbox.org>
2023-06-07 17:52:45 +0200

    convert avro values to arrow record batches

--- kafka-iceberg-ingest/src/lib.rs
+++ kafka-iceberg-ingest/src/lib.rs
@@ -1,1 +1,24 @@
+use anyhow::anyhow;
+use apache_avro::types::Value;
+
+pub mod arrow;
 pub mod schema;
+
+pub fn value_to_record(
+    value: Value,
+    debezium: Option<usize>,
+) -> Result<Vec<(String, Value)>, anyhow::Error> {
+    if let Value::Record(mut record) = value {
+        if let Some(index) = debezium {
+            if let Value::Record(record) = record.remove(index).1 {
+                Ok(record)
+            } else {
+                Err(anyhow!("Failed to coerce avro value to record."))
+            }
+        } else {
+            Ok(record)
+        }
+    } else {
+        Err(anyhow!("Failed to coerce avro value to record."))
+    }
+}
--- kafka-iceberg-ingest/src/main.rs
+++ kafka-iceberg-ingest/src/main.rs
@@ -1,12 +1,15 @@
 use std::{env, sync::Arc};
 
-use apache_avro::from_avro_datum;
+use anyhow::anyhow;
+use apache_avro::{from_avro_datum, Schema};
 use futures::stream::{self, StreamExt, TryStreamExt};
-use kafka_iceberg_ingest::schema::{debezium_schema, get_value_schema};
+use kafka_iceberg_ingest::{
+    arrow::avro_to_arrow_batch,
+    schema::{debezium_schema, get_value_schema},
+    value_to_record,
+};
 use rdkafka::{
     consumer::{Consumer, StreamConsumer},
-    error::KafkaError,
-    types::RDKafkaErrorCode,
     ClientConfig, Message,
 };
 
@@ -63,32 +66,58 @@ pub async fn main() {
         .for_each_concurrent(None, |partition| {
             let consumer = consumer.clone();
             let value_schema = value_schema.clone();
+            let table_schema = table_schema.clone();
             let topic = topic.clone();
             async move {
                 let temp = consumer
                     .split_partition_queue(&topic, *partition)
                     .expect("Failed to get stream for partition {partition}")
                     .stream()
+                    .map_err(|_| anyhow!("Failed to create Kafka message stream."))
                     .and_then(|message| {
                         let value_schema = value_schema.clone();
                         async move {
                             match message.payload() {
                                 Some(bytes) => {
-                                    from_avro_datum(&value_schema, &mut bytes.split_at(9).1, None)
-                                        .map_err(|err| {
-                                            println!("{}", err);
-                                            KafkaError::MessageConsumption(
-                                                RDKafkaErrorCode::BadMessage,
-                                            )
-                                        })
+                                    let value = from_avro_datum(
+                                        &value_schema,
+                                        &mut bytes.split_at(9).1,
+                                        None,
+                                    )?;
+                                    let record = if debezium {
+                                        let index = if let &Schema::Record {
+                                            name: _name,
+                                            aliases: _aliases,
+                                            doc: _doc,
+                                            fields: _fields,
+                                            lookup,
+                                        } = &value_schema.as_ref()
+                                        {
+                                            lookup.get("after").cloned()
+                                        } else {
+                                            None
+                                        };
+                                        value_to_record(value, index)?
+                                    } else {
+                                        value_to_record(value, None)?
+                                    };
+                                    Ok(record)
                                 }
-                                None => Err(KafkaError::MessageConsumption(
-                                    RDKafkaErrorCode::BadMessage,
-                                )),
+                                None => Err(anyhow!("Message contains no payload.")),
                             }
                         }
                     })
                     .try_chunks(arrow_batch_size)
+                    .map_err(|_| anyhow!("Failed to create arrow record batches."))
+                    .and_then(|values| {
+                        let table_schema = table_schema.clone();
+                        async move {
+                            avro_to_arrow_batch(
+                                &table_schema,
+                                values.iter().collect::<Vec<_>>().as_slice(),
+                            )
+                        }
+                    })
                     .try_collect::<Vec<_>>()
                     .await
                     .unwrap();
